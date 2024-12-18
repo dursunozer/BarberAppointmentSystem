3c431c884da2238f1eb27add74addc0746d98df7
ozer <dursun669@outlook.com>
2024-12-19 00:28:30 +0300

    I added sidebar to admin panel

--- Program.cs
+++ Program.cs
@@ -39,6 +39,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Admin}/{action=Dashboard}/{id?}");
 
 app.Run();
